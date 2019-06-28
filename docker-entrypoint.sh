54776ae2e0b4c17aebcf0a37292f7dd706ef7ee8
whopo <12553452+whopo@users.noreply.github.com>
2019-06-29 01:16:06 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,9 +8,4 @@ conf=${CONF_FILE:-"/config.json"}
 host=${HOST:-"0.0.0.0"}
 port=${PORT:-5000}
 
-
-if [[ "$*" == python*-m*youtube_dl_webui* ]]; then
-    exec gosu $puid:$pgid "$@" -c $conf --host $host --port $port
-fi
-
 exec "$@" -c $conf --host $host --port $port
